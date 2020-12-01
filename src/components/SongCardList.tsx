36aeca973ebdd6443d0b74590750659270782254
rockerishFox <delia.csiki.dely@gmail.com>
2020-12-01 18:27:07 +0200

    implement filtering by downloaded option

--- src/components/SongCardList.tsx
+++ src/components/SongCardList.tsx
@@ -11,7 +11,10 @@ import {
   IonFab,
   IonFabButton,
   IonButton,
+  IonItem,
+  IonLabel,
   IonIcon,
+  IonCheckbox,
 } from "@ionic/react";
 import Item from "./SongCard";
 import { ItemContext } from "./SongProvider";
@@ -28,6 +31,7 @@ const log = getLogger("ItemList");
 const ItemList: React.FC<RouteComponentProps> = ({ history }) => {
   const { items, fetching, fetchingError } = useContext(ItemContext);
   const [filteredItems, setFilteredItems] = useState<Song[]>([]);
+  const [filterByDownloaded, setFilterByDownloaded] = useState<boolean>(false);
   const { logout } = useContext(AuthContext);
   const [searchText, setSearchText] = useState<string>("");
   log("render");
@@ -37,11 +41,19 @@ const ItemList: React.FC<RouteComponentProps> = ({ history }) => {
   const content = filteredItems && (
     <IonList>
       {filteredItems
-        .filter(
-          (i) =>
-            i.name.toLowerCase().includes(searchText) ||
-            i.artist.toLowerCase().includes(searchText)
-        )
+        .filter((i) => {
+          if (filterByDownloaded) {
+            return (
+              (i.name.toLowerCase().includes(searchText) ||
+                i.artist.toLowerCase().includes(searchText)) &&
+              i.downloaded
+            );
+          } else
+            return (
+              i.name.toLowerCase().includes(searchText) ||
+              i.artist.toLowerCase().includes(searchText)
+            );
+        })
         .map(({ _id, name, artist, releaseDate, downloaded }) => (
           <Item
             key={_id}
@@ -61,7 +73,14 @@ const ItemList: React.FC<RouteComponentProps> = ({ history }) => {
       <IonHeader>
         <IonToolbar id="song-cardlist-toolbar">
           <IonTitle>Your songs</IonTitle>
-          <IonButton id="song-cardlist-logoutBtn" fill="outline" color="danger" onClick={logout}>Logout</IonButton>
+          <IonButton
+            id="song-cardlist-logoutBtn"
+            fill="outline"
+            color="danger"
+            onClick={logout}
+          >
+            Logout
+          </IonButton>
         </IonToolbar>
       </IonHeader>
 
@@ -69,9 +88,19 @@ const ItemList: React.FC<RouteComponentProps> = ({ history }) => {
         <IonSearchbar
           placeholder="Search by song name or artist"
           onIonChange={(event) =>
-            setSearchText(event.detail.value ? event.detail.value : "")
+            setSearchText(event.detail.value ? event.detail.value.toLowerCase() : "")
           }
         ></IonSearchbar>
+        <IonItem>
+          <IonCheckbox
+            color="danger"
+            style={{marginRight:"1rem"}}
+            checked={filterByDownloaded}
+            onIonChange={(e) => setFilterByDownloaded(e.detail.checked)}
+          />
+          <IonLabel> Show downloaded songs</IonLabel>
+        </IonItem>
+
         {fetching ? <Spinner show={fetching} /> : content}
         {fetchingError && (
           <div>{fetchingError.message || "Failed to fetch songs"}</div>
